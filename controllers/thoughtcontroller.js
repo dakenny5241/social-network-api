74b075ea74cf6bbedc47f49351ac6672d6602c83
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-06 11:56:24 +0100

    thoughtcontroller.js updated

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -117,6 +117,26 @@ const thoughtcontroller = {
         }
     
     },
+
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId},
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { new: true }
+                );
+
+            if (!thought) {
+                res.status(404).json({ message: 'Thought not found' });
+                return;
+            }
+
+            res.status(200).json(thought);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
 
 module.exports = thoughtcontroller;
