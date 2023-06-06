f880afaca39704e08479ece5e6008151a39efe97
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-06 11:10:33 +0100

    userroutes coded

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+});
+
+// Create the 'friendCount' virtual
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
