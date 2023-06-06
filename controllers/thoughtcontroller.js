a42c0a63d3054c48185a6f8fc53cde94727c1b81
dakenny5241 <123331009+dakenny5241@users.noreply.github.com>
2023-06-06 11:42:18 +0100

    thoughtcontroller.js updated

--- controllers/thoughtcontroller.js
+++ controllers/thoughtcontroller.js
@@ -28,6 +28,28 @@ const thoughtcontroller = {
             res.status(500).json(err);
         }
     },
+
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+
+            const user = await User.findOneAndUpdate(
+                { _id: req.body.userId},
+                { $push: { thoughts: thought._id } },
+                { new: true }
+                );
+
+            if (!user) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+
+            res.status(200).json(thought);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 }
 
 module.exports = thoughtcontroller;
